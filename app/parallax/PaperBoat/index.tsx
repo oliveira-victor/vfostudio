a05913d9321654a97fe8435dc31af84ab7654937
oliveira-victor <victor.freire.oliveira@gmail.com>
2024-11-15 15:58:08 -0300

    changing mobile hero height

--- app/parallax/PaperBoat/index.tsx
+++ app/parallax/PaperBoat/index.tsx
@@ -21,7 +21,7 @@ export default function PaperBoat() {
             style={{
                 position: 'relative',
                 width: '100%',
-                height: '100vh',
+                height: '100%',
                 overflow: 'hidden',
                 backgroundImage: 'url(/parallax1-bg.webp)'
             }}
